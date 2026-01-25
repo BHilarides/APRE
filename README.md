41bf09ca5362002164e36fefd2212f59da537e84
Ben Hilarides <ben.hilarides26@gmail.com>
2026-01-24 22:58:20 -0500

    Initial commit with sales by customer API/unit tests

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>WEB 450</h1>
+<h2>Contributors</h2>
+<ul>
+  <li>Richard Krasso</li>
+  <li>Ben Hilarides</li>
+</ul>
+
+<h3>Testing: npm test/ng test</h3>
+<h3>Run: npm run/ng serve</h3>
---
+++ apre-server/src/routes/reports/sales/index.js
@@ -0,0 +1,125 @@
+/**
+ * Author: Professor Krasso/Ben Hilarides
+ * Date: 8/14/24 | 1/24/26
+ * File: index.js
+ * Description: Apre sales report API for the sales reports
+ * Update: Added API to fetch sales data by customer
+ */
+
+'use strict';
+
+const express = require('express');
+const { mongo } = require('../../../utils/mongo');
+
+const router = express.Router();
+
+/**
+ * @description
+ *
+ * GET /regions
+ *
+ * Fetches a list of distinct sales regions.
+ *
+ * Example:
+ * fetch('/regions')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get('/regions', (req, res, next) => {
+  try {
+    mongo (async db => {
+      const regions = await db.collection('sales').distinct('region');
+      res.send(regions);
+    }, next);
+  } catch (err) {
+    console.error('Error getting regions: ', err);
+    next(err);
+  }
+});
+
+/**
+ * @description
+ *
+ * GET /regions/:region
+ *
+ * Fetches sales data for a specific region, grouped by salesperson.
+ *
+ * Example:
+ * fetch('/regions/north')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get('/regions/:region', (req, res, next) => {
+  try {
+    mongo (async db => {
+      const salesReportByRegion = await db.collection('sales').aggregate([
+        { $match: { region: req.params.region } },
+        {
+          $group: {
+            _id: '$salesperson',
+            totalSales: { $sum: '$amount'}
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            salesperson: '$_id',
+            totalSales: 1
+          }
+        },
+        {
+          $sort: { salesperson: 1 }
+        }
+      ]).toArray();
+      res.send(salesReportByRegion);
+    }, next);
+  } catch (err) {
+    console.error('Error getting sales data for region: ', err);
+    next(err);
+  }
+});
+
+/**
+ * @description
+ * 
+ * GET /customers/:customerId
+ *
+ * Fetches sales data for a specific customer.
+ *
+ * Example:
+ * fetch('/customers/CUST001')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get('/customers/:customerId', (req, res, next) => {
+  try {
+    mongo (async db => {
+      const salesReportByCustomer = await db.collection('sales').aggregate([
+        { $match: { customerId: req.params.customerId } },
+        {
+          $group: {
+            _id: '$customerId',
+            customerName: { $first: '$customerName' },
+            totalSales: { $sum: '$amount' },
+            orderCount: { $sum: 1 }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            customerId: '$_id',
+            customerName: 1,
+            totalSales: 1,
+            orderCount: 1
+          }
+        }
+      ]).toArray();
+      res.send(salesReportByCustomer);
+    }, next);
+  } catch (err) {
+    console.error('Error getting sales data for customer: ', err);
+    next(err);
+  }
+});
+
+module.exports = router;
